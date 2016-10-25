bce5beacc01f95213c1d00f263ea78e892c6e1b7
Lucas Hosseini <lucas.hosseini@gmail.com>
2016-10-25 06:34:50 +0200

    Fix serializable resource extensions.

--- lib/jsonapi/rails/deserializable/resource_extensions.rb
+++ lib/jsonapi/rails/deserializable/resource_extensions.rb
@@ -6,13 +6,13 @@ module JSONAPI
       module ResourceExtensions
         def deserialize_has_one_rel!(rel, &block)
           id = rel['data'] && rel['data']['id']
-          type = rel['data'] && rel['data']['type'].singularize.constantize
+          type = rel['data'] && rel['data']['type'].singularize.camelize
           instance_exec(rel, id, type, &block)
         end
 
         def deserialize_has_many_rel!(rel, &block)
           ids = rel['data'].map { |ri| ri['id'] }
-          types = rel['data'].map { |ri| ri['type'].singularize.constantize }
+          types = rel['data'].map { |ri| ri['type'].singularize.camelize }
           instance_exec(rel, ids, types, &block)
         end
       end
